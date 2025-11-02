5ecf76ce0388be32ca9f7dfbc66b86484cfeff20
Tushar More <tusharmore938@gmail.com>
2025-11-02 13:10:51 +0530

    added logo

--- frontend/src/layout/AppLayout.tsx
+++ frontend/src/layout/AppLayout.tsx
@@ -1,13 +1,16 @@
 import { Layout } from "antd";
 import { Content, Header } from "antd/es/layout/layout";
-import React from "react";
+import { FaInstagram } from "react-icons/fa";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <Layout className="min-h-screen">
-      <Header className="bg-secondary-400 border-b border-secondary-100">
-        Header
+      <Header className="bg-secondary-400 border-b border-secondary-100 items-start flex align-middle">
+      <div className="self-center rounded-xl bg-gradient-primary p-2 shadow-lg ">
+      <FaInstagram size={28}/>
+      </div> 
+      <h1 className="text-white p-2 self-center text-2xl">Commentor</h1>
       </Header>
       <Content className="h-full bg-secondary">{<Outlet />}</Content>
     </Layout>
